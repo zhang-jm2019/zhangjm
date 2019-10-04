626740b590cd3d49eaf87b447dc2c1b7bed9fb2d
zhang-jm2019 <54837094+zhang-jm2019@users.noreply.github.com>
2019-10-04 13:57:52 +0800

    Delete ex03.java

--- ex03.java
+++
@@ -1,9 +0,0 @@
-package ex1;
-
-//ComputeExpression.java
-public class ex03 {
-	public static void main(String[] args) {
-		System.out.print("(10.5 + 2*3)/(45 - 3.5) = ");
-		System.out.println((10.5 + 2 *3) / (45 - 3.5));
-	}
-}

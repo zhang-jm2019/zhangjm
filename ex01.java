408a2bb95d44c794ccad4e7d98720ba9bd36bbb7
zhang-jm2019 <54837094+zhang-jm2019@users.noreply.github.com>
2019-10-04 13:57:32 +0800

    Delete ex01.java

--- ex01.java
+++
@@ -1,9 +0,0 @@
-package ex1;
-
-//Welcome Java
-public class ex01 {
-	public static void main(String[] args) {
-		//Display message Welcome to Java!on the console
-		System.out.println("Welcome to Java!");
-	}
-}

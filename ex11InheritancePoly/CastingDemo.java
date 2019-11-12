8883aa904291d1acc4244606d53d37a1c5d44ba7
zhang-jm2019 <54837094+zhang-jm2019@users.noreply.github.com>
2019-11-12 15:19:50 +0800

    Add files via upload

---
+++ ex11InheritancePoly/CastingDemo.java
@@ -0,0 +1,28 @@
+package ex11InheritancePoly;
+
+public class CastingDemo {
+    /** Main method */
+    public static void main(String[] args) {
+     // Create and initialize two objects
+     Object objectl = new CircleFromSimpleGeometricObject(1);
+     Object object2 = new RectangleFromSimpleGeometricObject(1, 1);
+
+     // Display circle and rectangle
+      displayObject(objectl);
+      displayObject(object2);
+    }
+   
+    /** A method for displaying an object */
+    public static void displayObject(Object object) {
+        if (object instanceof CircleFromSimpleGeometricObject) {
+           System.out.println("The circle area is " +
+              ((CircleFromSimpleGeometricObject)object).getArea());
+           System.out.println("The circle diameter is " +
+              ((CircleFromSimpleGeometricObject)object).getDiameter());
+        }
+        else if (object instanceof RectangleFromSimpleGeometricObject) {
+           System.out.println("The rectangle area is " +
+             ((RectangleFromSimpleGeometricObject)object).getArea());
+        }
+    }
+}
---
+++ ex11InheritancePoly/DistinctNumbers.java
@@ -0,0 +1,24 @@
+package ex11InheritancePoly;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class DistinctNumbers {
+    public static void main(String[] args) {
+       ArrayList<Integer> list = new ArrayList<>();
+       
+       Scanner input=new Scanner(System.in);
+       System.out.print("Enter integers (input ends with 0): ");
+       int value;
+       
+       do {
+         value = input.nextInt(); // Read a value from the input
+         
+         if (!list.contains(value) && value != 0)
+            list.add(value); // Add the value if it is not in the *1ist
+       } while (value != 0);
+
+      // Display the distinct numbers
+      for (int i = 0; i < list.size(); i++)
+          System.out.print(list.get(i) + " ");
+    }
+}
